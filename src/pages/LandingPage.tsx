73a43d2d735bcdd863c35bf869d6b67ca07a770a
jgqy93 <jgqy93@gmail.com>
2022-10-13 09:19:28 +0800

    link to login on landing page added

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div>
       <h1>TeachSpace</h1>
+      <Link to="/login">Login</Link>
     </div>
   );
 };
